1029f7b44f46a67dedb288690263d946ca20f0e8
Adam Wentz <adam@adamwentz.com>
2023-03-17 21:51:00 -0700

    add option to return arrays instead of making files

--- wav2lip/inference.py
+++ wav2lip/inference.py
@@ -159,7 +159,61 @@ class Wav2LipInference:
         self.model = model.to(self.device)
         return self.model.eval()
 
-    def lipsync(self, face_filename, audio_filename, output_filename, tmpdir=None):
+    def lipsync(self, full_frames, wav):
+        """
+        Full frames should be a list of numpy arrays dtype=uint8
+        wav should be sampled at 16000hz
+        """
+        fps = self.fps
+
+        mel = audio.melspectrogram(wav)
+        print(mel.shape)
+
+        if np.isnan(mel.reshape(-1)).sum() > 0:
+            raise ValueError(
+                'Mel contains nan! Using a TTS voice? '
+                'Add a small epsilon noise to the wav file and try again'
+            )
+
+        mel_chunks = []
+        mel_idx_multiplier = 80. / fps
+        i = 0
+        while 1:
+            start_idx = int(i * mel_idx_multiplier)
+            if start_idx + self.mel_step_size > len(mel[0]):
+                mel_chunks.append(mel[:, len(mel[0]) - self.mel_step_size:])
+                break
+            mel_chunks.append(mel[:, start_idx: start_idx + self.mel_step_size])
+            i += 1
+
+        print("Length of mel chunks: {}".format(len(mel_chunks)))
+
+        full_frames = full_frames[:len(mel_chunks)]
+
+        batch_size = self.wav2lip_batch_size
+        gen = self.datagen(full_frames.copy(), mel_chunks, is_static)
+        frame_h, frame_w = full_frames[0].shape[:-1]
+        output_frames = []
+        for i, (img_batch, mel_batch, frames, coords) in enumerate(
+            tqdm(gen, total=int(np.ceil(float(len(mel_chunks)) / batch_size)))
+        ):
+            img_batch = torch.FloatTensor(np.transpose(img_batch, (0, 3, 1, 2))).to(self.device)
+            mel_batch = torch.FloatTensor(np.transpose(mel_batch, (0, 3, 1, 2))).to(self.device)
+
+            with torch.no_grad():
+                pred = self.model(mel_batch, img_batch)
+
+            pred = pred.cpu().numpy().transpose(0, 2, 3, 1) * 255.
+
+            for p, f, c in zip(pred, frames, coords):
+                y1, y2, x1, x2 = c
+                p = cv2.resize(p.astype(np.uint8), (x2 - x1, y2 - y1))
+
+                f[y1:y2, x1:x2] = p
+                output_frames.append(f)
+        return output_frames
+
+    def lipsync_file(self, face_filename, audio_filename, output_filename, tmpdir=None):
         if not os.path.isfile(face_filename):
             raise ValueError('--face argument must be a valid path to video/image file')
 
